0c3a501c89ab2c80dae975ab55e3b8e266c3bcd3
guiferviz <guiferviz@gmail.com>
2015-09-02 18:06:59 +0200

    Error fixed.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # encoding=utf-8
 
 
+import json
+import logging
 import webapp2
 
 import appengine_config
 from google.appengine.api import modules
+from google.appengine.api import app_identity
 
 import telegram
 
@@ -15,11 +18,12 @@ import telegram_token
 BOT = telegram.Bot(token=telegram_token.TOKEN)
 BOT_ME = BOT.getMe()
 
-# Setting the webhook (callback).
-VERSION = modules.get_current_version_name()
-HOST_NAME = modules.get_hostname()
-HOST_URL = 'https://{}-dot-{}/'.format(VERSION, HOST_NAME)
-BOT.setWebhook(HOST_URL)
+if not appengine_config.DEBUG:
+    # Setting the webhook (callback).
+    VERSION = modules.get_current_version_name()
+    HOST_NAME = app_identity.get_default_version_hostname()
+    HOST_URL = 'https://{}-dot-{}/{}'.format(VERSION, HOST_NAME, telegram_token.TOKEN)
+    BOT.setWebhook(HOST_URL)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -27,21 +31,20 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(BOT_ME)
+        self.response.write('\n')
         self.response.write(HOST_URL)
 
     def post(self):
-        # retrieve the message in JSON and then transform it to Telegram object
-        update = telegram.Update.de_json(self.request.get_json(force=True))
-
+        json_str = self.request.body
+        logging.info("POST Body: " + json_str)
+        json_obj = json.loads(json_str)
+        update = telegram.Update.de_json(json_obj)
         chat_id = update.message.chat.id
-
-        # Telegram understands UTF-8, so encode text for unicode compatibility
         text = update.message.text.encode('utf-8')
-
-        # repeat the same message back (echo)
-        BOT.sendMessage(chat_id=chat_id, text=text)
-
+        if text != '':
+            logging.info("Message Text: " + text)
+            BOT.sendMessage(chat_id=chat_id, text=text)
 
 app = webapp2.WSGIApplication([
-    ('/.*', MainPage),
+    ('/' + telegram_token.TOKEN, MainPage),
 ], debug=appengine_config.DEBUG)
