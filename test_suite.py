ce0c9ab7cab78bfec349b661a475250036fbbc2d
guiferviz <guiferviz@gmail.com>
2015-09-04 19:36:08 +0200

    Improved test_suite.py.

--- test_suite.py
+++ test_suite.py
@@ -2,19 +2,42 @@
 
 import os
 import unittest
+import webtest
 
 import dev_appserver
-
-
-# Folder where test will search.
-TEST_DIR = os.path.join(os.path.dirname(__file__), 'tests')
 
 # Fix the sys.path to include GAE extra paths.
 dev_appserver.fix_sys_path()
 
+from google.appengine.ext import testbed
 
-# Run all tests.
-def main():
+import main
+
+
+# Folder where test will be search.
+TEST_DIR = os.path.join(os.path.dirname(__file__), 'tests')
+
+
+class AppEngineTestBase(unittest.TestCase):
+    """
+    Base class for AppEngine tests.
+    """
+
+    def setUp(self):
+        # Create and activate Testbed to moock AppEngine APIs.
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        # Create app for testing handlers.
+        self.testapp = webtest.TestApp(main.app)
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+
+def runAllTests():
+    """
+    Run all tests.
+    """
     loader = unittest.TestLoader()
     test_suite = loader.discover(TEST_DIR, pattern='*')
     unittest.TextTestRunner(verbosity=2).run(test_suite)
@@ -22,4 +45,4 @@ def main():
 
 # If it's the main file we run all the tests under 'tests' module.
 if __name__ == '__main__':
-    main()
+    runAllTests()
