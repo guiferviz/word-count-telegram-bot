aa7212c75a6c80eea46138ce406c24a6fbff93df
guiferviz <guiferviz@gmail.com>
2015-09-08 01:39:49 +0200

    Using service for send the response.

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from google.appengine.api import app_identity
 import telegram
 
 import telegram_token
+from service import WordCountService
 
 
 # Creating the bot and getting basic info of it.
@@ -24,6 +25,14 @@ if not appengine_config.DEBUG:
     HOST_NAME = app_identity.get_default_version_hostname()
     HOST_URL = 'https://{}-dot-{}/{}'.format(VERSION, HOST_NAME, telegram_token.TOKEN)
     BOT.setWebhook(HOST_URL)
+
+# Text to send back after substitution.
+RESPONSE_TEXT = """Letters: %(letters)s
+Words: %(words)s
+Lines: %(lines)s
+"""
+
+RESPONSE_NO_TEXT = "Please, send me some text. " + telegram.Emoji.PAGE_FACING_UP
 
 
 class MainPage(webapp2.RequestHandler):
@@ -44,11 +53,22 @@ class MainPage(webapp2.RequestHandler):
             text = update.message.text.encode('utf-8')
             if text != '':
                 logging.info("Message Text: " + text)
-                BOT.sendMessage(chat_id=chat_id, text=text)
+                BOT.sendMessage(chat_id=chat_id,
+                                text=self.get_response(text))
+            else:
+                logging.info("No Message Text.")
+                BOT.sendMessage(chat_id=chat_id,
+                                text=self.get_response())
         except ValueError:
             logging.error('No body or bad JSON body.')
         except AttributeError:
             logging.error('No correct attributes in JSON body.')
+
+    def get_response(self, text=None):
+        if text:
+            return RESPONSE_TEXT % WordCountService.count(text)
+
+        return RESPONSE_NO_TEXT
 
 app = webapp2.WSGIApplication([
     ('/' + telegram_token.TOKEN, MainPage),
